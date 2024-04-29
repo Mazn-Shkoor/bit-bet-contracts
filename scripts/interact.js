13b9115c95527677da11147515448211a6c8b529
Mazn-Shkoor <mazn.adnan.shkoor@gmail.com>
2024-04-29 07:45:39 +0300

    added event listener for the transactions

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat");
+const { ethers, artifacts } = require("hardhat");
 const crypto = require("crypto");
 require("dotenv").config();
 
@@ -36,17 +36,78 @@ async function main() {
         value: amountToSend,
       });
 
-      const receipt = await tx.wait();
+      // get deployed contract
+      const myContract = await ethers.getContractAt(
+        Dice.interface,
+        DEPLOYED_CONTRACT_ADDRESS
+      );
 
-      for (const event of receipt.events) {
-        console.log(`Event ${event.event} with args ${event.args}`);
-      }
+      const filter = myContract.filters.BetPlaced();
+      const filterRandomNumber = myContract.filters.RandomNumber();
+
+      // Subscribe to the event
+      myContract.on(filter, (eventData) => {
+        const args = eventData.args;
+
+        const player = args[0];
+        const amount = args[1];
+        const target = args[2];
+        const betDirection = args[3];
+        const multiplier = args[4];
+        const payout = args[5];
+        const randomNumber = args[6];
+        const result = args[7];
+
+        console.log("BetPlaced Event:");
+        console.log("-------------------");
+
+        console.log("Player:", player);
+        console.log("Amount:", ethers.formatEther(amount), "eth");
+        console.log("Target:", target);
+        console.log("Bet Direction:", betDirection);
+        console.log("Multiplier:", multiplier);
+        console.log("Payout:", ethers.formatEther(payout), "eth");
+        console.log("Random Number:", randomNumber);
+        console.log("Result:", result);
+      });
+
+      // Subscribe to the RandomNumber event
+      myContract.on(filterRandomNumber, (eventData) => {
+        const args = eventData.args;
+
+        const clientSeed = args[0];
+        const serverSeed = args[1];
+        const combinedSeed = args[2];
+        const hashSeed = args[3];
+        const firstTenHash = args[4];
+        const firstTenHashInt = args[5];
+        const randomNumber = args[6];
+
+        console.log("RandomNumber Event:");
+        console.log("-------------------");
+
+        console.log("Client Seed:", clientSeed);
+        console.log("Server Seed:", serverSeed);
+        console.log("Combined Seed:", combinedSeed);
+        console.log("Hash Seed:", hashSeed);
+        console.log("First Ten Hash:", firstTenHash);
+        console.log("First Ten Hash Int:", firstTenHashInt);
+        console.log("Random Number:", randomNumber);
+      });
+
+      // Wait for the event
+      await new Promise((resolve) => setTimeout(resolve, 30000)); // Wait for half a minute
+
+      // Unsubscribe from the event
+      myContract.removeAllListeners();
+
+      // console.log(tx);
     } catch (error) {
       console.error(error.message);
     }
 
     // try {
-    //   const amountInWei = ethers.parseEther("0.1"); // Withdraw 1 ETH, specify amount in Ether
+    //   const amountInWei = ethers.parseEther("0.6"); // Withdraw 1 ETH, specify amount in Ether
     //   const transaction = await contract.withdrawHouseFunds(amountInWei);
     //   // const withdraw = await contract.withdrawHouseFunds(1000000000000000000);
     //   console.log(transaction.hash);
